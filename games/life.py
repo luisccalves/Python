9f0675fda8b27c0800f2d7d951fe41616c59d291
luisccalves <luisccalves@sapo.pt>
2022-08-24 17:30:04 +0100

    Update life.py

---
+++ games/life.py
@@ -0,0 +1,14 @@
+# https://python-course.eu/tkinter/the-game-of-life-in-python.php
+
+import sys
+import tkinter
+from dataclasses import dataclass
+from random import random
+
+__update__ = ''
+
+if sys.version_info.major == 3 and sys.version_info.minor >= 9:
+    listtype = list
+else:
+    from typing import List
+    listtype = List
